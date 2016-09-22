3abe547d50095230aeb7bacad7957c72a1a8c11b
Frozenlight <oleast@stud.ntnu.no>
2016-09-22 17:34:41 +0200

    Added Swig as standard template engine, added views for testing of template engine

--- mongoose-test/app.js
+++ mongoose-test/app.js
@@ -3,7 +3,9 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var swig = require('swig');
 var Scene = require('./models/Scene.js')
+
 
 // Use native Node promises
 //mongoose.Promise = global.Promise;
@@ -17,6 +19,39 @@ app.use( bodyParser.json() );       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
+
+
+// Set swig to be the standard template engine for express
+var swig = new swig.Swig();
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
+
+var router = express.Router();
+
+router.use(function(req,res,next){
+	console.log('Something is happening.');
+	next()
+})
+
+app.use('/', router);
+
+router.route('/scenes')
+	.post(function(res,req){
+		Scene.create()
+		res.json({message:'Scene created!'})
+	})
+	.get(function(req,res){
+		Scene.find(function(err, scenes){
+			if (err){
+				res.send(err);
+			}
+			res.render('scene-table', {scenes:scenes,title:'List of scenes'});
+		});
+	});
+
+router.get('/', function(req, res) {
+    res.json({ message: 'hooray! welcome to our api!' });   
+});
 
 app.get('/form',function(req,res){
 	res.sendFile(__dirname + '/templates/form.html');
@@ -36,7 +71,7 @@ app.post('/form', function(req, res) {
 
 });
 
-app.get('/storsalen', function(req,res){
+app.get('/:', function(req,res){
 	Scene.find({name:'storsalen'},function(err,data){
 		if (err) return console.error(err);
   		console.log(JSON.stringify(data));
