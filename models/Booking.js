96f0dde903212c249b1b76b79a3cecf9a36257ed
MountainPancake <z.b.svela@gmail.com>
2016-10-10 10:38:08 +0200

    Made booking-model

---
+++ models/Booking.js
@@ -0,0 +1,14 @@
+
+var mongoose = require('mongoose');
+var shortid = require('shortid');
+
+var BookingSchema = new mongoose.Schema({
+	_id: {type:String,'default':shortid.generate},
+	email: String,
+	text: String,
+	approval: Boolean,
+	price: Number,
+	date: String,
+});
+
+module.exports = mongoose.model('Booking', BookingSchema);
